83456caf9d10354c08b778e6fd20c33bbd123a00
williambza <william.brander@gmail.com>
2021-07-22 10:49:19 +0200

    Create repro

---
+++ Endpoint2/CompleteOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class CompleteOrderHandler :
+    IHandleMessages<CompleteOrder>
+{
+    static int processCount = 0;
+    static ILog log = LogManager.GetLogger<CompleteOrderHandler>();
+
+    public Task Handle(CompleteOrder message, IMessageHandlerContext context)
+    {
+        if (processCount++ == 0)
+        {
+            // Fail on the first attempt
+            throw new System.NotImplementedException();
+        }
+
+        log.Info($"Received CompleteOrder with credit card number {message.CreditCard}");
+        return Task.CompletedTask;
+    }
+}
---
+++ Endpoint2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.MessageBodyEncryption.Endpoint2";
+        var endpointConfiguration = new EndpointConfiguration("Samples.MessageBodyEncryption.Endpoint2");
+
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        transport.ConnectionString("[Connection string]");
+
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.Recoverability().Immediate(a => a.NumberOfRetries(0));
+        endpointConfiguration.Recoverability().Delayed(a => a.NumberOfRetries(0));
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        endpointConfiguration.RegisterMessageEncryptor();
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
